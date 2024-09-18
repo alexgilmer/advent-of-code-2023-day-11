6481499ec8fe00809c29f623408329d02917dbb3
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-09-18 17:09:30 -0500

    Solve part 1

--- 2023-day-11/Program.cs
+++ 2023-day-11/Program.cs
@@ -4,7 +4,97 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool useTestData = false;
+            IList<string> data = useTestData ? GetTestInput() : GetPuzzleInput();
+
+            bool[] rowsWithStars = new bool[data.Count];
+            bool[] colsWithStars = new bool[data[0].Length];
+            List<KeyValuePair<int, int>> galaxies = [];
+            
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (data[i][j] == '#')
+                    {
+                        rowsWithStars[i] = true;
+                        colsWithStars[j] = true;
+                        galaxies.Add(new(i, j));
+                    }
+                }
+            }
+
+            Dictionary<KeyValuePair<int, int>, int> rowCalculations = [];
+            Dictionary<KeyValuePair<int, int>, int> colCalculations = [];
+
+            int result = 0;
+            int connections = 0;
+
+            for (int i = 0; i < galaxies.Count - 1; i++)
+            {
+                for (int j = i + 1; j < galaxies.Count; j++)
+                {
+                    KeyValuePair<int, int> rowRange = GetRange(galaxies[i].Key, galaxies[j].Key);
+                    KeyValuePair<int, int> colRange = GetRange(galaxies[i].Value, galaxies[j].Value);
+
+                    if (rowCalculations.TryGetValue(rowRange, out int rowValue))
+                    {
+                        result += rowValue;
+                    }
+                    else
+                    {
+                        rowCalculations[rowRange] = rowsWithStars[rowRange.Key..rowRange.Value].Sum(t => t ? 1 : 2);
+                        result += rowCalculations[rowRange];
+                    }
+
+                    if (colCalculations.TryGetValue(colRange, out int colValue))
+                    {
+                        result += colValue;
+                    }
+                    else
+                    {
+                        colCalculations[colRange] = colsWithStars[colRange.Key..colRange.Value].Sum(t => t ? 1 : 2);
+                        result += colCalculations[colRange];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Distance sum: {result}.  Connections: {connections}");
+        }
+
+        static IList<string> GetPuzzleInput()
+        {
+            string file = Path.Combine(Environment.CurrentDirectory, "puzzle-input.txt");
+            using StreamReader sr = new StreamReader(file);
+            List<string> input = [];
+
+            while (!sr.EndOfStream)
+            {
+                input.Add(sr.ReadLine()!);
+            }
+
+            return input;
+        }
+
+        static IList<string> GetTestInput()
+        {
+            return @"...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....".Split('\n');
+        }
+
+        static KeyValuePair<int, int> GetRange(int a, int b)
+        {
+            if (a > b)
+                return new(b, a);
+            return new(a, b);
         }
     }
 }

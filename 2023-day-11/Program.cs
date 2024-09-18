0a32416fdedc10c0044e3f4109ac8eeccce61de9
Alex Gilmer <alex.gilmer.business@gmail.com>
2024-09-18 16:37:11 -0500

    Add project files.

---
+++ 2023-day-11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2023_day_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
